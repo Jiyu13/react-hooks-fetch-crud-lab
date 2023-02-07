de3c14a74ff2c861e3d5a8c615d64cae1b57e85e
Ziru Zou <jiyucoding@yahoo.com>
2023-02-07 18:54:14 -0500

    refactor object for body in fetch request, should put answers together

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -20,13 +20,18 @@ function QuestionForm( { onAddNewQuestion } ) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData)
+    
+    const data = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex,
+    }
 
     // POST request
     fetch("http://localhost:4000/questions", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(formData)
+      body: JSON.stringify(data)
     })
     .then(res => res.json())
     .then(newQuestion => onAddNewQuestion(newQuestion))
