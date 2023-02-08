68838bca2e63c1a7be8df1ff370aa51aa28741cc
Ziru Zou <jiyucoding@yahoo.com>
2023-02-07 19:22:22 -0500

    add onChange event to <select>, create handleChange with PATCH reqeust , call CB to pass updatedQuestion as arg

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function QuestionItem({ question, onDeleteQuestion }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
-  
-  const options = answers?.map((answer, index) => (
+
+  const options = answers?.map((answer, index) => ( // add "?" in case answers is blank
     <option key={index} value={index}>
       {answer}
     </option>
@@ -16,13 +16,27 @@ function QuestionItem({ question, onDeleteQuestion }) {
     .then(() => onDeleteQuestion(question))
   }
 
+
+  function handleChange(e) {
+    const updatedAnswerIndex = e.target.value
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "PATCH",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        correctIndex: updatedAnswerIndex
+      })
+    })
+    .then(res => res.json())
+    .then(updatedQuestion => onUpdateQuestion(updatedQuestion))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
       <button onClick={handleDelete}>Delete Question</button>
     </li>
